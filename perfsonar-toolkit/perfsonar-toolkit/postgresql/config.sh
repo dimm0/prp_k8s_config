8f2a64d8f3112f38ab4c951f158f17a1a5085c30
Dmitry Mishin <dmitry.mishin@gmail.com>
2018-04-05 20:21:18 -0700

    Right way to copy pgsql config in perfsonar ma

--- perfsonar-toolkit/perfsonar-toolkit/postgresql/config.sh
+++ perfsonar-toolkit/perfsonar-toolkit/postgresql/config.sh
@@ -2,9 +2,9 @@
 
 # Overlay the configuration files
 if [ ! -f /var/lib/pgsql/$PG_VERSION/data/init_done ]; then
-  su - postgres -p -c "/usr/pgsql-9.5/bin/pg_ctl initdb"
-  cp ./postgresql.conf /var/lib/pgsql/$PG_VERSION/data/postgresql.conf
-  cp ./pg_hba.conf /var/lib/pgsql/$PG_VERSION/data/pg_hba.conf
+  su - postgres -c "/usr/pgsql-9.5/bin/pg_ctl initdb"
+  su - postgres -c "cp -f /opt/postgresql/postgresql.conf /var/lib/pgsql/$PG_VERSION/data/"
+  su - postgres -c "cp -f /opt/postgresql/pg_hba.conf /var/lib/pgsql/$PG_VERSION/data/"
 
   /opt/postgresql/esmond-build-database
   touch /var/lib/pgsql/$PG_VERSION/data/init_done

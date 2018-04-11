9ab5afbb8be0f35b333f121aa2295533bb1dd42f
John Graham <jjgraham@ucsd.edu>
2018-04-11 01:28:16 -0700

    Update jupyterhub_config.py

--- dhub/jupyterhub_config.py
+++ dhub/jupyterhub_config.py
@@ -40,38 +40,17 @@ class LocalCILogonOAuthenticator(LocalAuthenticator, CILogonOAuthenticator):
 c.JupyterHub.authenticator_class = LocalCILogonOAuthenticator
 c.LocalCILogonOAuthenticator.create_system_users = True
 c.LocalCILogonOAuthenticator.add_user_cmd = ['adduser']
-c.Authenticator.admin_users = {'dmishinucsdedu', 'jjgrahamucsdedu'}
+c.Authenticator.admin_users = {'dmishin', 'jjgraham'}
 c.JupyterHub.admin_access = True
 
+## Spawner
 from jupyterhub.spawner import LocalProcessSpawner
 from wrapspawner import ProfilesSpawner
-## Spawner
 from dockerspawner import SystemUserSpawner
-#c.JupyterHub.spawner_class = SystemUserSpawner
 c.JupyterHub.spawner_class = ProfilesSpawner
-#c.DockerSpawner.container_image = "systemuser:datascience-notebook-zonca-caffe-cuda-7.5-cudnn5-runtime"
-#c.DockerSpawner.container_image = "jupyterhub/systemuser"
-#c.DockerSpawner.container_image = "gpu_jupyterhub/singleuser"
-#c.DockerSpawner.remove_containers = True
-
-## nvidia-docker
-# [zonca@patternlab dockerspawner]$ curl -s localhost:3476/docker/cli
-#--volume-driver=nvidia-docker --volume=nvidia_driver_361.93.02:/usr/local/nvidia:ro --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia-uvm-tools --device=/dev/nvidia0 --device=/dev/nvidia1
-#--volume-driver=nvidia-docker --volume=nvidia_driver_367.55:/usr/local/nvidia:ro --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia-uvm-tools --device=/dev/nvidia0 --device=/dev/nvidia1[zonca@patternlab run_jupyterhub]$ 
-
-#c.DockerSpawner.read_only_volumes = {"nvidia_driver_367.55":"/usr/local/nvidia"}
-#c.DockerSpawner.extra_create_kwargs = {"volume_driver":"nvidia-docker"}
-#c.DockerSpawner.extra_host_config = { "devices":["/dev/nvidiactl","/dev/nvidia-uvm","/dev/nvidia-uvm-tools","/dev/nvidia0","/dev/nvidia1"] }
 
 c.ProfilesSpawner.profiles = [
       ( "Host process", 'local', 'jupyterhub.spawner.LocalProcessSpawner', {'ip':'0.0.0.0'} ),
-      #('Docker CAFFE', 'caffe', 'dockerspawner.SystemUserSpawner',
-      #   dict(
-      #      container_image="systemuser:datascience-notebook-zonca-caffe-cuda-7.5-cudnn5-runtime",
-      #      read_only_volumes = {"nvidia_driver_361.93.02":"/usr/local/nvidia"},
-      #      extra_create_kwargs = {"volume_driver":"nvidia-docker"},
-      #      extra_host_config = { "devices":["/dev/nvidiactl","/dev/nvidia-uvm","/dev/nvidia-uvm-tools","/dev/nvidia0","/dev/nvidia1"] }
-      #   )),
       ('Docker CPU-only', 'systemuser', 'dockerspawner.SystemUserSpawner',
          dict(container_image="jupyterhub/systemuser")),
 ]

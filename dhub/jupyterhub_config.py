778145fe9b34ac3aae9c408cd5ef04cd57657a59
John Graham <jjgraham@ucsd.edu>
2018-04-13 06:57:38 -0700

    Update jupyterhub_config.py

--- dhub/jupyterhub_config.py
+++ dhub/jupyterhub_config.py
@@ -14,6 +14,7 @@ class LocalCILogonOAuthenticator(LocalAuthenticator, CILogonOAuthenticator):
     """A version that mixes in local system user creation"""
     def normalize_username(self, username):       
         return username.replace('@', '.').lower()
+        return username.replace(".","")
     
 c.JupyterHub.authenticator_class = LocalCILogonOAuthenticator
 c.LocalCILogonOAuthenticator.create_system_users = True

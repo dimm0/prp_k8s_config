8fcfd952083809720ae4e24ca30adb691891e8c5
John Graham <jjgraham@ucsd.edu>
2018-04-13 06:17:43 -0700

    Update jupyterhub_config.py

--- dhub/jupyterhub_config.py
+++ dhub/jupyterhub_config.py
@@ -8,34 +8,13 @@ c.JupyterHub.hub_ip = '127.0.0.1'
 from oauthenticator.cilogon import CILogonOAuthenticator
 from jupyterhub.auth import LocalAuthenticator
 from oauthenticator.cilogon import *
+c.CILogonOAuthenticator.username_claim = 'email'
+
 class LocalCILogonOAuthenticator(LocalAuthenticator, CILogonOAuthenticator):
     """A version that mixes in local system user creation"""
-    def normalize_username(self, username):
-        """Turn a user token into a username"""
-        uri = url_concat(ujoin(self.oauth_url, 'getcert'), {
-            'oauth_token': token,
-        })
-        uri, _, _ = self.oauth_client.sign(uri)
-        resp = yield self.client.fetch(uri)
-        # FIXME: handle failure
-        reply = resp.body.decode('utf8', 'replace')
-        _, cert_txt = reply.split('\n', 1)
-        cert = load_certificate(FILETYPE_PEM, cert_txt)
-        username = None
-        for i in range(cert.get_extension_count()):
-            ext = cert.get_extension(i)
-            if ext.get_short_name().decode('ascii', 'replace') == 'subjectAltName':
-                data = ext.get_data()
-                # cert starts with some weird bytes. Not sure why or if they are consistent
-                username = data[4:].decode('utf8').lower()
-                # workaround notebook bug not handling @
-                username = username.replace('@', '.')
-                break
-        if username is None:
-            raise ValueError("Failed to get username from cert: %s", cert_txt)
-       
+    def normalize_username(self, username):       
         return username.replace('@', '.').lower(), cert_txt
-
+    
 c.JupyterHub.authenticator_class = LocalCILogonOAuthenticator
 c.LocalCILogonOAuthenticator.create_system_users = True
 c.LocalCILogonOAuthenticator.add_user_cmd = ['adduser']

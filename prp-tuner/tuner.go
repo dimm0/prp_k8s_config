12c05ce9e9b7df3b44620737d855d0329198b4fd
Dmitry Mishin <dmitry.mishin@gmail.com>
2018-05-04 11:00:06 -0700

    No FQ limit in tuner

--- prp-tuner/tuner.go
+++ prp-tuner/tuner.go
@@ -54,63 +54,12 @@ func adjustAll() {
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 	conf := &NodeConfig{}
 
-	if fq, err := getFQMaxRate(); err == nil {
-		conf.FQ = fmt.Sprintf("%.2fgbit", float64(fq)*8.0/1000000000.0)
-	}
-
 	b, err := json.Marshal(conf)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	w.Write(b)
-}
-
-func getFQMaxRate() (uint32, error) {
-	link, _ := getDefaultInterface()
-	qdiscs, err := netlink.QdiscList(link)
-	if err != nil {
-		return 0, err
-	}
-
-	fq, ok := qdiscs[0].(*netlink.Fq)
-	if !ok {
-		return 0, nil
-	}
-
-	// fmt.Printf("Flow: %v", fq.FlowMaxRate)
-
-	return fq.FlowMaxRate, nil
-}
-
-func adjustFQMaxRate() error {
-	link, _ := getDefaultInterface()
-	if speed, err := getInterfaceSpeed(link.Attrs().Name); err != nil {
-		return err
-	} else {
-		if speed >= 10000 {
-			fqMaxRateDesired := uint32(speed / 16 / 8 * 1000000)
-			if fqMaxRate, err := getFQMaxRate(); err != nil || fqMaxRate != fqMaxRateDesired {
-				log.Printf("Adjusting FQ MaxRate from %v to %v", fqMaxRate, fqMaxRateDesired)
-				qdiscs, _ := netlink.QdiscList(link)
-				for _, qdisk := range qdiscs {
-					netlink.QdiscDel(qdisk)
-				}
-
-				qdisc := &netlink.Fq{
-					QdiscAttrs: netlink.QdiscAttrs{
-						LinkIndex: link.Attrs().Index,
-						Handle:    netlink.MakeHandle(1, 0),
-						Parent:    netlink.HANDLE_ROOT,
-					},
-					FlowMaxRate: fqMaxRateDesired,
-				}
-
-				netlink.QdiscAdd(qdisc)
-			}
-		}
-		return nil
-	}
 }
 
 func getDefaultInterface() (netlink.Link, error) {

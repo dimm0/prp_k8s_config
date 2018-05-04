8b6faa6398e6ad51dcf5699dd9c474dbb4ee3b29
Dmitry Mishin <dmitry.mishin@gmail.com>
2018-05-03 20:26:57 -0700

    prptuner is tested and deployed on cluster

--- prp-tuner/tuner.go
+++ prp-tuner/tuner.go
@@ -1,7 +1,5 @@
 package main
 
-//https://github.com/golang/go/issues/22688
-
 import (
 	"encoding/json"
 	"fmt"
@@ -11,6 +9,8 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
+	"github.com/spf13/viper"
 
 	"github.com/vishvananda/netlink"
 )
@@ -20,26 +20,34 @@ type NodeConfig struct {
 }
 
 func main() {
-	// getFQMaxRate()
-	// ticker := time.NewTicker(10 * time.Minute)
-	// go func() {
-	// 	adjustAll()
-	// 	for {
-	// 		select {
-	// 		case <-ticker.C:
-	// 			adjustAll()
-	// 		}
-	// 	}
-	// }()
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %s", err))
+	}
 
-	// http.HandleFunc("/", RootHandler)
-	// log.Fatal(http.ListenAndServe(":10015", nil))
-	adjustAll()
+	ticker := time.NewTicker(10 * time.Minute)
+	go func() {
+		adjustAll()
+		for {
+			select {
+			case <-ticker.C:
+				adjustAll()
+			}
+		}
+	}()
+
+	http.HandleFunc("/", RootHandler)
+	log.Fatal(http.ListenAndServe(":10015", nil))
 }
 
 func adjustAll() {
 	if err := adjustFQMaxRate(); err != nil {
 		log.Printf("Error adjusting FQ: %s", err.Error())
+	}
+	if err := adjustSysctlSettings(); err != nil {
+		log.Printf("Error adjusting sysctl: %s", err.Error())
 	}
 }
 
@@ -83,6 +91,7 @@ func adjustFQMaxRate() error {
 		if speed >= 10000 {
 			fqMaxRateDesired := uint32(speed / 16 / 8 * 1000000)
 			if fqMaxRate, err := getFQMaxRate(); err != nil || fqMaxRate != fqMaxRateDesired {
+				log.Printf("Adjusting FQ MaxRate from %v to %v", fqMaxRate, fqMaxRateDesired)
 				qdiscs, _ := netlink.QdiscList(link)
 				for _, qdisk := range qdiscs {
 					netlink.QdiscDel(qdisk)
@@ -94,9 +103,6 @@ func adjustFQMaxRate() error {
 						Handle:    netlink.MakeHandle(1, 0),
 						Parent:    netlink.HANDLE_ROOT,
 					},
-					// Rate:   131072,
-					// Limit:  1220703,
-					// Buffer: 16793,
 					FlowMaxRate: fqMaxRateDesired,
 				}
 
@@ -114,16 +120,34 @@ func getDefaultInterface() (netlink.Link, error) {
 	return link, nil
 }
 
-func getInterfaceSpeed(interf string) (int, error) {
+func getInterfaceSpeed(interf string) (uint32, error) {
 	if b, err := ioutil.ReadFile(fmt.Sprintf("/sys/class/net/%s/speed", interf)); err != nil {
 		return 0, err
 	} else {
 		speedStr := fmt.Sprintf("%s", b)
 		speedStr = strings.TrimRight(speedStr, "\r\n")
-		if i, err := strconv.Atoi(speedStr); err != nil {
+		if i, err := strconv.ParseUint(speedStr, 10, 32); err != nil {
 			return 0, err
 		} else {
-			return i, nil
+			return uint32(i), nil
 		}
 	}
+}
+
+func adjustSysctlSettings() error {
+	for key, val := range viper.Get("sysctl").(map[string]interface{}) {
+		path := fmt.Sprintf("/proc/sys/%s", strings.Replace(key, ".", "/", -1))
+		if b, err := ioutil.ReadFile(path); err != nil {
+			return err
+		} else {
+			fileValue := strings.TrimRight(fmt.Sprintf("%s", b), "\r\n")
+			if fileValue != val.(string) {
+				fmt.Printf("Adjusting sysctl value %s from %s to %s\n", key, fileValue, val.(string))
+				if err := ioutil.WriteFile(path, []byte(val.(string)), 0644); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
 }

ed579b3734c62cbe571397983edc4cf90e878697
John Graham <jjgraham@eng.ucsd.edu>
2018-06-22 14:33:36 +0000

    Add new file

---
+++ globus-connect/load.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python2 /usr/bin/esmond-ps-load-gridftp -p gridftp.pickle -U https://nrp-perfsonar.nautilus.optiputer.net/esmond -u gridftp -k 3db5dbd582fb4316f8be96138f39b15cb46cd713 -f /var/log/gridftp-transfer.log 

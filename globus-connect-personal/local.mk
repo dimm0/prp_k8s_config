1d60095f169812dc2efcf9a63f7efe0430bd074c
nadyawilliams <nadya@sdsc.edu>
2018-10-25 17:02:12 -0700

    initial

---
+++ globus-connect-personal/local.mk
@@ -0,0 +1,8 @@
+# default docker images registry
+REGISTRY='gateway.calit2.optiputer.net:5000/prp-k8s/'
+# volume size to use for mounting on a container
+SIZE=50
+# user namespace
+NAMESPACE=npw
+# physical node network speed
+NW=40

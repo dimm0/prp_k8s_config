67f44d9a66ab976c0635c1e7c8663d0a79d880ba
Dmitry Mishin <dmitry.mishin@gmail.com>
2018-10-26 13:06:19 -0700

    Moved to a separate repo

--- globus-connect-personal/local.mk
+++
@@ -1,8 +0,0 @@
-# default docker images registry
-REGISTRY='gateway.calit2.optiputer.net:5000/prp-k8s/'
-# volume size to use for mounting on a container
-SIZE=50
-# user namespace
-NAMESPACE=npw
-# physical node network speed
-NW=40
